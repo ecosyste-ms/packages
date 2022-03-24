16bd257fe59bd3a3ec55b409bbc9f6ef3ad90b65
Andrew Nesbitt <andrewnez@gmail.com>
2022-03-24 15:38:44 +0000

    Add support for npm packages

---
+++ app/models/ecosystem/npm.rb
@@ -0,0 +1,125 @@
+# frozen_string_literal: true
+
+module Ecosystem
+  class Npm < Base
+    def package_url(package, version = nil)
+      "https://www.npmjs.com/package/#{package.name}" + (version ? "/v/#{version}" : "")
+    end
+
+    def download_url(name, version = nil)
+      "#{@registry_url}/#{name}/-/#{name}-#{version}.tgz"
+    end
+
+    def install_command(package, version = nil)
+      "npm install #{package.name}" + (version ? "@#{version}" : "")
+    end
+
+    def formatted_name
+      "npm"
+    end
+
+    def all_package_names
+      get("https://raw.githubusercontent.com/nice-registry/all-the-package-names/master/names.json")
+    end
+
+    def recently_updated_package_names
+      u = "#{@registry_url}/-/rss?descending=true&limit=50"
+      SimpleRSS.parse(get_raw(u)).items.map(&:title).uniq
+    end
+
+    def fetch_package_metadata(name)
+      get("#{@registry_url}/#{name.gsub('/', '%2F')}")
+    end
+
+    def deprecation_info(name)
+      versions = package(name)["versions"].values
+
+      {
+        is_deprecated: versions.all? { |v| v["deprecated"] },
+        message: versions.last["deprecated"],
+      }
+    end
+
+    def map_package_metadata(package)
+      return false unless package["versions"].present?
+
+      latest_version = package["versions"].to_a.last[1]
+
+      repo = latest_version.fetch("repository", {})
+      repo = repo[0] if repo.is_a?(Array)
+      repo_url = repo.try(:fetch, "url", nil)
+
+      {
+        name: package["name"],
+        description: latest_version["description"],
+        homepage: package["homepage"],
+        keywords_array: Array.wrap(latest_version.fetch("keywords", [])),
+        licenses: licenses(latest_version),
+        repository_url: repo_fallback(repo_url, package["homepage"]),
+        versions: package["versions"],
+      }
+    end
+
+    def licenses(latest_version)
+      license = latest_version.fetch("license", nil)
+      if license.present?
+        if license.is_a?(Hash)
+          license.fetch("type", "")
+        else
+          license
+        end
+      else
+        licenses = Array(latest_version.fetch("licenses", []))
+        licenses.map do |lice|
+          if lice.is_a?(Hash)
+            lice.fetch("type", "")
+          else
+            lice
+          end
+        end.join(",")
+      end
+    end
+
+    def versions_metadata(package, _name)
+      # npm license fields are supposed to be SPDX expressions now https://docs.npmjs.com/files/package.json#license
+      package["versions"].map do |k, v|
+        license = v.fetch("license", nil)
+        license = licenses(v) unless license.is_a?(String)
+        {
+          number: k,
+          published_at: package.fetch("time", {}).fetch(k, nil),
+          original_license: license,
+        }
+      end
+    end
+
+    def dependencies_metadata(_name, version, package)
+      vers = package[:versions][version]
+      return [] if vers.nil?
+
+      map_dependencies(vers.fetch("dependencies", {}), "runtime") +
+        map_dependencies(vers.fetch("devDependencies", {}), "Development") +
+        map_dependencies(vers.fetch("optionalDependencies", {}), "Optional", true)
+    end
+
+    def dependents(name)
+      dependents = []
+      offset = 0
+      per_page = 36
+      url = "https://www.npmjs.com/browse/depended/#{name}"
+      while offset < 5000 do
+        page = get_html(url+"?offset=#{offset}")
+
+        names = page.css('.mb4.bt.b--black-10 section h3').map(&:text)
+
+        break if names.blank?
+
+        dependents += names
+
+        offset += per_page
+      end
+
+      return dependents
+    end
+  end
+end
---
+++ test/models/ecosystem/npm_test.rb
@@ -0,0 +1,66 @@
+require "test_helper"
+
+class NpmTest < ActiveSupport::TestCase
+  setup do
+    @registry = Registry.new(name: 'Npmjs.org', url: 'https://registry.npmjs.org', ecosystem: 'npm')
+    @ecosystem = Ecosystem::Npm.new(@registry.url)
+    @package = Package.new(ecosystem: 'npm', name: 'base62')
+    @version = @package.versions.build(number: '2.0.1')
+  end
+
+  test 'package_url' do
+    package_url = @ecosystem.package_url(@package)
+    assert_equal package_url, 'https://www.npmjs.com/package/base62'
+  end
+
+  test 'package_url with version' do
+    package_url = @ecosystem.package_url(@package, @version.number)
+    assert_equal package_url, 'https://www.npmjs.com/package/base62/v/2.0.1'
+  end
+
+  test 'download_url' do
+    download_url = @ecosystem.download_url(@package.name, @version.number)
+    assert_equal download_url, "https://registry.npmjs.org/base62/-/base62-2.0.1.tgz"
+  end
+
+  test 'documentation_url' do
+    documentation_url = @ecosystem.documentation_url(@package.name)
+    assert_nil documentation_url
+  end
+
+  test 'documentation_url with version' do
+    documentation_url = @ecosystem.documentation_url(@package.name, @version.number)
+    assert_nil documentation_url
+  end
+
+  test 'install_command' do
+    package_url = @ecosystem.install_command(@package)
+    assert_equal package_url, 'npm install base62'
+  end
+
+  test 'install_command with version' do
+    package_url = @ecosystem.install_command(@package, @version.number)
+    assert_equal package_url, 'npm install base62@2.0.1'
+  end
+
+  test 'check_status_url' do
+    check_status_url = @ecosystem.check_status_url(@package)
+    assert_equal check_status_url, "https://www.npmjs.com/package/base62"
+  end
+
+  test 'all_package_names' do
+    stub_request(:get, "https://raw.githubusercontent.com/nice-registry/all-the-package-names/master/names.json")
+      .to_return({ status: 200, body: file_fixture('npm/names.json') })
+    all_package_names = @ecosystem.all_package_names
+    assert_equal all_package_names.length, 290
+    assert_equal all_package_names.last, '03-creatfront'
+  end
+
+  test 'recently_updated_package_names' do
+    stub_request(:get, "https://registry.npmjs.org/-/rss?descending=true&limit=50")
+      .to_return({ status: 200, body: file_fixture('npm/new-rss') })
+    recently_updated_package_names = @ecosystem.recently_updated_package_names
+    assert_equal recently_updated_package_names.length, 45
+    assert_equal recently_updated_package_names.last, '@trafilea/afrodita-components'
+  end
+end
