3d510789da113a3a9f4ed35065a26ffb3b8562da
Andrew Nesbitt <andrewnez@gmail.com>
2022-03-25 10:48:56 +0000

    More packagist tests

--- test/models/ecosystem/packagist_test.rb
+++ test/models/ecosystem/packagist_test.rb
@@ -66,19 +66,49 @@ class PackagistTest < ActiveSupport::TestCase
     assert_equal recently_updated_package_names.last, 'pringuin/pimcore-lucene-search'
   end
 
-  test 'fetch_package_metadata' do
-    skip("To be implemented")
-  end
-
-  test 'map_package_metadata' do
-    skip("To be implemented")
+  test 'package_metadata' do
+    stub_request(:get, "https://packagist.org/packages/psr/log.json")
+      .to_return({ status: 200, body: file_fixture('packagist/log.json') })
+    package_metadata = @ecosystem.package_metadata('psr/log')
+    
+    assert_equal package_metadata[:name], "psr/log"
+    assert_equal package_metadata[:description], "Common interface for logging libraries"
+    assert_nil package_metadata[:homepage]
+    assert_equal package_metadata[:licenses], "MIT"
+    assert_equal package_metadata[:repository_url], "https://github.com/php-fig/log"
+    assert_equal package_metadata[:keywords_array], ["log", "psr", "psr-3"]
   end
 
   test 'versions_metadata' do
-    skip("To be implemented")
+    stub_request(:get, "https://packagist.org/packages/psr/log.json")
+      .to_return({ status: 200, body: file_fixture('packagist/log.json') })
+    package_metadata = @ecosystem.package_metadata('psr/log')
+    versions_metadata = @ecosystem.versions_metadata(package_metadata)
+
+    assert_equal versions_metadata, [
+      {:number=>"3.0.0", :published_at=>"2021-07-14T16:46:02+00:00"},
+      {:number=>"2.0.0", :published_at=>"2021-07-14T16:41:46+00:00"},
+      {:number=>"1.1.4", :published_at=>"2021-05-03T11:20:27+00:00"},
+      {:number=>"1.1.3", :published_at=>"2020-03-23T09:12:05+00:00"},
+      {:number=>"1.1.2", :published_at=>"2019-11-01T11:05:21+00:00"},
+      {:number=>"1.1.1", :published_at=>"2019-10-25T08:06:51+00:00"},
+      {:number=>"1.1.0", :published_at=>"2018-11-20T15:27:04+00:00"},
+      {:number=>"1.0.2", :published_at=>"2016-10-10T12:19:37+00:00"},
+      {:number=>"1.0.1", :published_at=>"2016-09-19T16:02:08+00:00"},
+      {:number=>"1.0.0", :published_at=>"2012-12-21T11:40:51+00:00"}
+    ]
   end
 
   test 'dependencies_metadata' do
-    skip("To be implemented")
+    stub_request(:get, "https://packagist.org/packages/maztech/instagram-php-graph-sdk.json")
+      .to_return({ status: 200, body: file_fixture('packagist/instagram-php-graph-sdk.json') })
+    package_metadata = @ecosystem.package_metadata('maztech/instagram-php-graph-sdk')
+    dependencies_metadata = @ecosystem.dependencies_metadata('maztech/instagram-php-graph-sdk', 'v1.0.0', package_metadata)
+    
+    assert_equal dependencies_metadata, [
+      {:package_name=>"phpunit/phpunit", :requirements=>"~4.0", :kind=>"Development", :optional=>false, :ecosystem=>"Packagist"},
+      {:package_name=>"mockery/mockery", :requirements=>"~0.8", :kind=>"Development", :optional=>false, :ecosystem=>"Packagist"},
+      {:package_name=>"guzzlehttp/guzzle", :requirements=>"~5.0", :kind=>"Development", :optional=>false, :ecosystem=>"Packagist"}
+    ]
   end
 end
