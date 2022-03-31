6175b1fbfe18e74bc06fa381245319c762cbfdbf
Andrew Nesbitt <andrewnez@gmail.com>
2022-03-31 17:34:53 +0100

    Add support for Spack ecosystem

---
+++ app/models/ecosystem/spack.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+module Ecosystem
+  class Spack < Base
+    def package_url(package, version = nil)
+      "#{@registry_url}/packages/package.html?name=#{package.name}"
+    end
+
+    def install_command(package, version = nil)
+      "spack install #{package.name}" + (version ? "@#{version}" : "")
+    end
+
+    def all_package_names
+      get_json("#{@registry_url}/packages/data/packages.json")
+    rescue StandardError
+      {}
+    end
+
+    def recently_updated_package_names
+      all_package_names
+    end
+
+    def fetch_package_metadata(name)
+      get_json("#{@registry_url}/packages/data/packages/#{name}.json")
+    rescue StandardError
+      {}
+    end
+
+    def map_package_metadata(package)
+      {
+        name: package["name"],
+        description: package["description"],
+        homepage: package["homepage"],
+        licenses: [],
+        repository_url: package["homepage"],
+        versions: package["versions"],
+        dependencies: package['dependencies']
+      }
+    end
+
+    def versions_metadata(package)
+      package[:versions].map do |v|
+        {
+          number: v["name"],
+        }
+      end
+    rescue StandardError
+      []
+    end
+
+    def dependencies_metadata(name, version, package)
+      return [] unless package[:dependencies]
+
+      package[:dependencies].map do |dep|
+        {
+          package_name: dep["name"],
+          requirements: '*',
+          kind: 'runtime',
+          platform: self.class.name.demodulize
+        }
+      end
+    rescue StandardError
+      []
+    end
+  end
+end
---
+++ test/models/ecosystem/spack_test.rb
@@ -0,0 +1,97 @@
+require "test_helper"
+
+class SpackTest < ActiveSupport::TestCase
+  setup do
+    @registry = Registry.new(name: 'Pypi.org', url: 'https://spack.github.io', ecosystem: 'spack')
+    @ecosystem = Ecosystem::Spack.new(@registry.url)
+    @package = Package.new(ecosystem: 'spack', name: '3proxy')
+    @version = @package.versions.build(number: '0.8.13')
+  end
+
+  test 'package_url' do
+    package_url = @ecosystem.package_url(@package)
+    assert_equal package_url, 'https://spack.github.io/packages/package.html?name=3proxy'
+  end
+
+  test 'package_url with version' do
+    package_url = @ecosystem.package_url(@package, @version.number)
+    assert_equal package_url, 'https://spack.github.io/packages/package.html?name=3proxy'
+  end
+
+  test 'download_url' do
+    download_url = @ecosystem.download_url(@package.name, @version.number)
+    assert_nil download_url
+  end
+
+  test 'documentation_url' do
+    documentation_url = @ecosystem.documentation_url(@package.name)
+    assert_nil documentation_url
+  end
+
+  test 'documentation_url with version' do
+    documentation_url = @ecosystem.documentation_url(@package.name, @version.number)
+    assert_nil documentation_url
+  end
+
+  test 'install_command' do
+    install_command = @ecosystem.install_command(@package)
+    assert_equal install_command, 'spack install 3proxy'
+  end
+
+  test 'install_command with version' do
+    install_command = @ecosystem.install_command(@package, @version.number)
+    assert_equal install_command, 'spack install 3proxy@0.8.13'
+  end
+
+  test 'all_package_names' do
+    stub_request(:get, "https://spack.github.io/packages/data/packages.json")
+      .to_return({ status: 200, body: file_fixture('spack/packages.json') })
+    all_package_names = @ecosystem.all_package_names
+    assert_equal all_package_names.length, 6371
+    assert_equal all_package_names.last, 'zziplib'
+  end
+
+  test 'recently_updated_package_names' do
+    stub_request(:get, "https://spack.github.io/packages/data/packages.json")
+      .to_return({ status: 200, body: file_fixture('spack/packages.json') })
+    recently_updated_package_names = @ecosystem.recently_updated_package_names
+    assert_equal recently_updated_package_names.length, 6371
+    assert_equal recently_updated_package_names.last, 'zziplib'
+  end
+
+  test 'package_metadata' do
+    stub_request(:get, "https://spack.github.io/packages/data/packages/3proxy.json")
+      .to_return({ status: 200, body: file_fixture('spack/3proxy.json') })
+    package_metadata = @ecosystem.package_metadata('3proxy')
+    
+    assert_equal package_metadata[:name], "3proxy"
+    assert_equal package_metadata[:description], "3proxy - tiny free proxy server\n"
+    assert_equal package_metadata[:homepage], "https://3proxy.org"
+    assert_equal package_metadata[:licenses], []
+    assert_equal package_metadata[:repository_url], "https://3proxy.org"
+    assert_nil package_metadata[:keywords_array]
+  end
+
+  test 'versions_metadata' do
+    stub_request(:get, "https://spack.github.io/packages/data/packages/3proxy.json")
+      .to_return({ status: 200, body: file_fixture('spack/3proxy.json') })
+    package_metadata = @ecosystem.package_metadata('3proxy')
+    versions_metadata = @ecosystem.versions_metadata(package_metadata)
+
+    assert_equal versions_metadata, [{:number=>"0.8.13"}, {:number=>"0.8.12"}, {:number=>"0.8.11"}]
+  end
+
+  test 'dependencies_metadata' do
+    stub_request(:get, "https://spack.github.io/packages/data/packages/3proxy.json")
+      .to_return({ status: 200, body: file_fixture('spack/3proxy.json') })
+    package_metadata = @ecosystem.package_metadata('3proxy')
+    dependencies_metadata = @ecosystem.dependencies_metadata('3proxy', '0.8.13', package_metadata)
+
+    assert_equal dependencies_metadata, [
+      {:package_name=>"autoconf", :requirements=>"*", :kind=>"runtime", :platform=>"Spack"},
+      {:package_name=>"automake", :requirements=>"*", :kind=>"runtime", :platform=>"Spack"},
+      {:package_name=>"libtool", :requirements=>"*", :kind=>"runtime", :platform=>"Spack"},
+      {:package_name=>"m4", :requirements=>"*", :kind=>"runtime", :platform=>"Spack"}
+    ]
+  end
+end
