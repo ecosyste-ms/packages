30e40dbb23b191581dab8e31186975a5e940d316
Andrew Nesbitt <andrewnez@gmail.com>
2022-04-05 15:53:41 +0100

    Sort versions by published_at

--- app/controllers/api/v1/versions_controller.rb
+++ app/controllers/api/v1/versions_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::VersionsController < Api::V1::ApplicationController
   def index
     @registry = Registry.find(params[:registry_id])
     @package = @registry.packages.find(params[:package_id])
-    @pagy, @versions = pagy(@package.versions.includes(:dependencies))
+    @pagy, @versions = pagy(@package.versions.includes(:dependencies).order('published_at desc, created_at desc'))
   end
 end
