744a69e10aaaa0d7faa88c9a4e798ac654a69c0d
Andrew Nesbitt <andrewnez@gmail.com>
2022-04-05 15:49:47 +0100

    Sort packages api list by latest_release_published_at

--- app/controllers/api/v1/packages_controller.rb
+++ app/controllers/api/v1/packages_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::PackagesController < Api::V1::ApplicationController
   def index
     # paginated list of packages for a registry
     @registry = Registry.find(params[:registry_id])
-    @pagy, @packages = pagy(@registry.packages)
+    @pagy, @packages = pagy(@registry.packages.order('latest_release_published_at DESC, created_at DESC'))
   end
 
   def show
