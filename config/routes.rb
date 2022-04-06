0b315112314e5e2e40b1009e0d660b90be587e4c
Andrew Nesbitt <andrewnez@gmail.com>
2022-04-06 14:22:25 +0100

    add basic auth for sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 require 'sidekiq/web'
 
+Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+  ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+end if Rails.env.production?
+
 Rails.application.routes.draw do
   mount Sidekiq::Web => "/sidekiq" # TODO password protect
   mount PgHero::Engine, at: "pghero" # TODO password protect
