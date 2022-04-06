f7582ff6d5dee9cb7f9f85eabf5c37873edc4747
Andrew Nesbitt <andrewnez@gmail.com>
2022-04-06 14:48:38 +0100

    downcase registry names

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
 default_registries = [
-  {name: 'Bower.io', url: 'https://bower.io', ecosystem: 'bower', github: 'bower'},
-  {name: 'Crates.io', url: 'https://crates.io', ecosystem: 'Cargo', github: 'rust-lang'},
-  {name: 'Cocoapod.org', url: 'https://cocoapods.org', ecosystem: 'Cocoapods', github: 'Cocoapods'},
-  {name: 'Metacpan.org', url: 'https://metacpan.org', ecosystem: 'cpan', github: 'metacpan'},
-  {name: 'proxy.golang.org', url: 'https://proxy.golang.org', ecosystem: 'Go', github: 'golang'},
-  {name: 'Hex.pm', url: 'https://hex.pm', ecosystem: 'Hex', github: 'hexpm'},
-  {name: 'Npmjs.org', url: 'https://registry.npmjs.org', ecosystem: 'npm', github: 'npm'},
-  {name: 'NuGet.org', url: 'https://www.nuget.org', ecosystem: 'nuget', github: 'nuget'},
-  {name: 'Packagist.org', url: 'https://packagist.org', ecosystem: 'Packagist', github: 'Packagist'},
-  {name: 'Pub.dev', url: 'https://pub.dev', ecosystem: 'pub', github: 'dart-lang'},
-  {name: 'Pypi.org', url: 'https://pypi.org', ecosystem: 'pypi', github: 'pypi'},
-  {name: 'Rubygems.org', url: 'https://rubygems.org', ecosystem: 'rubygems', github: 'rubygems'},
-  {name: 'Spack.io', url: 'https://spack.github.io', ecosystem: 'spack', github: 'spack'},
-  {name: 'Hackage.haskell.org', url: 'https://hackage.haskell.org', ecosystem: 'hackage', github: 'haskell'}
+  {name: 'bower.io', url: 'https://bower.io', ecosystem: 'bower', github: 'bower'},
+  {name: 'crates.io', url: 'https://crates.io', ecosystem: 'cargo', github: 'rust-lang'},
+  {name: 'cocoapod.org', url: 'https://cocoapods.org', ecosystem: 'cocoapods', github: 'cocoapods'},
+  {name: 'metacpan.org', url: 'https://metacpan.org', ecosystem: 'cpan', github: 'metacpan'},
+  {name: 'proxy.golang.org', url: 'https://proxy.golang.org', ecosystem: 'go', github: 'golang'},
+  {name: 'hex.pm', url: 'https://hex.pm', ecosystem: 'hex', github: 'hexpm'},
+  {name: 'npmjs.org', url: 'https://registry.npmjs.org', ecosystem: 'npm', github: 'npm'},
+  {name: 'nuget.org', url: 'https://www.nuget.org', ecosystem: 'nuget', github: 'nuget'},
+  {name: 'packagist.org', url: 'https://packagist.org', ecosystem: 'packagist', github: 'packagist'},
+  {name: 'pub.dev', url: 'https://pub.dev', ecosystem: 'pub', github: 'dart-lang'},
+  {name: 'pypi.org', url: 'https://pypi.org', ecosystem: 'pypi', github: 'pypi'},
+  {name: 'rubygems.org', url: 'https://rubygems.org', ecosystem: 'rubygems', github: 'rubygems'},
+  {name: 'spack.io', url: 'https://spack.github.io', ecosystem: 'spack', github: 'spack'},
+  {name: 'hackage.haskell.org', url: 'https://hackage.haskell.org', ecosystem: 'hackage', github: 'haskell'}
 ]
 
 default_registries.each do |registry|
