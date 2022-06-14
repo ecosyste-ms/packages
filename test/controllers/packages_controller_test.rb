5dc00a0ac3f17ad38464686fb6b766898abcd041
Andrew Nesbitt <andrewnez@gmail.com>
2022-06-14 13:53:45 +0100

    packages controller tests

---
+++ test/controllers/packages_controller_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class PackagesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @registry = Registry.create(name: 'crates.io', url: 'https://crates.io', ecosystem: 'cargo')
+    @package = @registry.packages.create(ecosystem: 'cargo', name: 'rand', metadata: {foo: 'bar'})
+  end
+
+  test 'list packages for a registry' do
+    get registry_packages_path(registry_id: @registry.name)
+    assert_response :success
+    assert_template 'packages/index', file: 'packages/index.html.erb'
+  end
+
+  test 'get a package for a registry' do
+    get registry_package_path(registry_id: @registry.name, id: @package.name)
+    assert_response :success
+    assert_template 'packages/show', file: 'packages/show.html.erb'
+  end
+end
