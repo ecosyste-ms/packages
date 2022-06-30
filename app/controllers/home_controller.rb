a1470a90d3ab86ab1568b97f59da2d8f36971e19
Andrew Nesbitt <andrewnez@gmail.com>
2022-06-30 16:58:38 +0100

    Ensure versions with far future dates don't break graphs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @recent_versions = Version.where('published_at > ?', 2.months.ago).group_by_day(:published_at).count
+    @recent_versions = Version.where('published_at > ?', 2.months.ago).where('published_at < ?', Time.now).group_by_day(:published_at).count
     @registries = Registry.order('packages_count desc').all
   end
 end
