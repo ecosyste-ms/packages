9ebb839651d576958acb36b4c064310e3cdcc6e3
Andrew Nesbitt <andrewnez@gmail.com>
2022-07-28 10:42:45 +0100

    Update dependencies

--- test/models/ecosystem/maven_test.rb
+++ test/models/ecosystem/maven_test.rb
@@ -91,8 +91,8 @@ class MavenTest < ActiveSupport::TestCase
     versions_metadata = @ecosystem.versions_metadata(package_metadata)
 
     assert_equal versions_metadata, [
-      {:number=>"5.17.102.7", :published_at=>"2022-07-12 12:10:25 +0000", :licenses=>["APL2"]},
-      {:number=>"5.17.225.2", :published_at=>"2022-07-12 12:10:25 +0000", :licenses=>["APL2"]}
+      {:number=>"5.17.102.7", :published_at=>"2022-07-12 12:10:25 +0000", :licenses=>"APL2"},
+      {:number=>"5.17.225.2", :published_at=>"2022-07-12 12:10:25 +0000", :licenses=>"APL2"}
     ]
   end
 
