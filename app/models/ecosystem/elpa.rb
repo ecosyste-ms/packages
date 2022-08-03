2b24abac1045db69feeaba22e5c3a1041fea02dc
Andrew Nesbitt <andrewnez@gmail.com>
2022-08-03 15:15:51 +0100

    Add support for elpa (gnu and nongnu)

---
+++ app/models/ecosystem/elpa.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+module Ecosystem
+  class Elpa < Base
+    def registry_url(package, version = nil)
+      "#{@registry_url}/#{package.name}.html"
+    end
+
+    def download_url(package, version)
+      return nil unless version.present?
+      "#{@registry_url}/#{package.name}-#{version}.tar"
+    end
+
+    def install_command(package, version = nil)
+      "M-x package-install RET #{package.name} RET"
+    end
+
+    def all_package_names
+      get_html("#{@registry_url}").css('table td a:first').map(&:text)
+    rescue
+      []
+    end
+
+    def recently_updated_package_names
+      all_package_names
+    end
+
+    def fetch_package_metadata(name)
+      page = get_html("#{@registry_url}/#{name}.html", headers: { "Accept" => "text/html" })
+      {
+        name: name,
+        page: page
+      }
+    end
+
+    def extract_fields(page)
+      keys = page.css('dl').first.css('dt').map(&:text)
+      values = page.css('dl').first.css('dd').map(&:text)
+      Hash[keys.zip(values)] 
+    end
+
+    def map_package_metadata(package)
+      return nil if package[:page].blank?
+      fields = extract_fields(package[:page])
+      {
+        name: package[:name],
+        description: fields["Description"],
+        homepage: fields["Home page"],
+        repository_url: repo_fallback(fields["Home page"], ''),
+        page: package[:page]
+      }
+    end
+
+    def versions_metadata(package)
+      fields = extract_fields(package[:page])
+      filename = fields['Latest'].split(',')[0]
+      date = fields['Latest'].split(', ')[1]
+      version = filename.gsub("#{package[:name]}-",'').gsub('.tar','')
+      [{ number: version, published_at: date }]
+    end
+  end
+end
---
+++ test/models/ecosystem/elpa_test.rb
@@ -0,0 +1,99 @@
+require "test_helper"
+
+class ElpaTest < ActiveSupport::TestCase
+  setup do
+    @registry = Registry.new(name: 'elpa.nongnu.org', url: 'https://elpa.nongnu.org/nongnu', ecosystem: 'elpa')
+    @ecosystem = Ecosystem::Elpa.new(@registry.url)
+    @package = Package.new(ecosystem: 'elpa', name: 'ample-theme')
+    @version = @package.versions.build(number: '0.3.0')
+  end
+
+  test 'registry_url' do
+    registry_url = @ecosystem.registry_url(@package)
+    assert_equal registry_url, 'https://elpa.nongnu.org/nongnu/ample-theme.html'
+  end
+
+  test 'registry_url with version' do
+    registry_url = @ecosystem.registry_url(@package, @version)
+    assert_equal registry_url, 'https://elpa.nongnu.org/nongnu/ample-theme.html'
+  end
+
+  test 'download_url' do
+    download_url = @ecosystem.download_url(@package, @version)
+    assert_equal download_url, 'https://elpa.nongnu.org/nongnu/ample-theme-0.3.0.tar'
+  end
+
+  test 'documentation_url' do
+    documentation_url = @ecosystem.documentation_url(@package)
+    assert_nil documentation_url
+  end
+
+  test 'documentation_url with version' do
+    documentation_url = @ecosystem.documentation_url(@package, @version.number)
+    assert_nil documentation_url
+  end
+
+  test 'install_command' do
+    install_command = @ecosystem.install_command(@package)
+    assert_equal install_command, 'M-x package-install RET ample-theme RET'
+  end
+
+  test 'install_command with version' do
+    install_command = @ecosystem.install_command(@package, @version.number)
+    assert_equal install_command, 'M-x package-install RET ample-theme RET'
+  end
+
+  test 'check_status_url' do
+    check_status_url = @ecosystem.check_status_url(@package)
+    assert_equal check_status_url, 'https://elpa.nongnu.org/nongnu/ample-theme.html'
+  end
+
+  test 'all_package_names' do
+    stub_request(:get, "https://elpa.nongnu.org/nongnu")
+      .to_return({ status: 200, body: file_fixture('elpa/index.html') })
+    all_package_names = @ecosystem.all_package_names
+    assert_equal all_package_names.length, 177
+    assert_equal all_package_names.last, 'zig-mode'
+  end
+
+  test 'recently_updated_package_names' do
+    stub_request(:get, "https://elpa.nongnu.org/nongnu")
+      .to_return({ status: 200, body: file_fixture('elpa/index.html') })
+    recently_updated_package_names = @ecosystem.recently_updated_package_names
+    assert_equal recently_updated_package_names.length, 177
+    assert_equal recently_updated_package_names.last, 'zig-mode'
+  end
+
+  test 'package_metadata' do
+    stub_request(:get, "https://elpa.nongnu.org/nongnu/ample-theme.html")
+      .to_return({ status: 200, body: file_fixture('elpa/ample-theme.html') })
+    package_metadata = @ecosystem.package_metadata('ample-theme')
+    
+    assert_equal package_metadata[:name], "ample-theme"
+    assert_equal package_metadata[:description], "Calm Dark Theme for Emacs"
+    assert_equal package_metadata[:homepage], "https://github.com/jordonbiondo/ample-theme"
+    assert_nil package_metadata[:licenses]
+    assert_equal package_metadata[:repository_url], "https://github.com/jordonbiondo/ample-theme"
+    assert_nil package_metadata[:keywords_array]
+  end
+
+  test 'versions_metadata' do
+    stub_request(:get, "https://elpa.nongnu.org/nongnu/ample-theme.html")
+      .to_return({ status: 200, body: file_fixture('elpa/ample-theme.html') })
+    package_metadata = @ecosystem.package_metadata('ample-theme')
+    versions_metadata = @ecosystem.versions_metadata(package_metadata)
+
+    assert_equal versions_metadata, [
+      {
+        :number=>"0.3.0", 
+        :published_at=>"2021-Oct-22"
+      }
+    ]
+  end
+
+  test 'dependencies_metadata' do
+    dependencies_metadata = @ecosystem.dependencies_metadata('ample-theme', '0.3.0', nil)
+
+    assert_equal dependencies_metadata, []
+  end
+end
