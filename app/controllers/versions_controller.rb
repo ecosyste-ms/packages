200d0f2e9a913cbf3b20e3136e53b2b6ce9b48bd
Andrew Nesbitt <andrewnez@gmail.com>
2022-09-01 14:50:58 +0100

    List dependencies on versions#show

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -2,7 +2,7 @@ class VersionsController < ApplicationController
   def show
     @registry = Registry.find_by_name!(params[:registry_id])
     @package = @registry.packages.find_by_name!(params[:package_id])
-    @version = @package.versions.find_by_number(params[:id])
+    @version = @package.versions.includes(:dependencies).find_by_number(params[:id])
   end
 
   def recent
