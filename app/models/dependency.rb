7def924618e15666462a4c8c290cf78b1f0b52b1
Andrew Nesbitt <andrewnez@gmail.com>
2022-09-26 14:27:54 +0100

    Add ecosystem scope to dependencies

--- app/models/dependency.rb
+++ app/models/dependency.rb
@@ -3,4 +3,6 @@ class Dependency < ApplicationRecord
   belongs_to :package, optional: true
 
   validates_presence_of :package_name, :version_id, :requirements, :ecosystem
+
+  scope :ecosystem, ->(ecosystem) { where(ecosystem: Ecosystem::Base.format_name(ecosystem)) }
 end
