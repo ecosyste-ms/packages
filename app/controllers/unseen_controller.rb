19c4c73129befa1e0655a4c0ae4ca9cfec2f77f9
Andrew Nesbitt <andrewnez@gmail.com>
2022-10-04 14:32:00 +0100

    Use countless pagination for unseen pkg lists

--- app/controllers/unseen_controller.rb
+++ app/controllers/unseen_controller.rb
@@ -8,6 +8,6 @@ class UnseenController < ApplicationController
     @ecosystem = params[:ecosystem]
     @registry = Registry.where(ecosystem: @ecosystem).first
     @scope = @registry.packages.with_repo_metadata.order('downloads DESC').where('downloads > ?', 100_000).where("(repo_metadata ->> 'stargazers_count')::text::integer < 100")
-    @pagy, @packages = pagy(@scope)
+    @pagy, @packages = pagy_countless(@scope)
   end
 end
