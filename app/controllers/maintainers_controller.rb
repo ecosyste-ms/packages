ea7112d2680ad83a07337dd42f36684bb65270f4
Andrew Nesbitt <andrewnez@gmail.com>
2022-11-07 12:25:56 +0000

    Better sorting on maintainer lists

--- app/controllers/maintainers_controller.rb
+++ app/controllers/maintainers_controller.rb
@@ -1,12 +1,12 @@
 class MaintainersController < ApplicationController
   def index
     @registry = Registry.find_by!(name: params[:registry_id])
-    @pagy, @maintainers = pagy(@registry.maintainers)
+    @pagy, @maintainers = pagy(@registry.maintainers.order('packages_count DESC'))
   end
 
   def show
     @registry = Registry.find_by!(name: params[:registry_id])
     @maintainer = @registry.maintainers.find_by!(login: params[:id])
-    @pagy, @packages = pagy(@maintainer.packages)
+    @pagy, @packages = pagy(@maintainer.packages.order('latest_release_published_at DESC'))
   end
 end
