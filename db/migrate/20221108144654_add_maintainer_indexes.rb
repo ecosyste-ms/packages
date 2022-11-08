5776069977e6c9bcf8cf8bc4be80f7e50f329ddb
Andrew Nesbitt <andrewnez@gmail.com>
2022-11-08 15:52:17 +0000

    unique again

--- db/migrate/20221108144654_add_maintainer_indexes.rb
+++ db/migrate/20221108144654_add_maintainer_indexes.rb
@@ -1,6 +1,6 @@
 class AddMaintainerIndexes < ActiveRecord::Migration[7.0]
   def change
-    add_index :maintainers, [:registry_id, :login]
+    add_index :maintainers, [:registry_id, :login], unique: true
     add_index :maintainers, [:registry_id, :uuid], unique: true
 
     add_index :maintainerships, :maintainer_id
