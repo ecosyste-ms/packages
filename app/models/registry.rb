95f60a82dfa6cef95dde34b4d6448a6f4fc7c995
Andrew Nesbitt <andrewnez@gmail.com>
2023-02-01 15:37:23 +0000

    Disable update_dependent_package_ids for now, too slow

--- app/models/registry.rb
+++ app/models/registry.rb
@@ -162,7 +162,7 @@ class Registry < ApplicationRecord
 
     package.update(versions_count: package.versions.count, last_synced_at: Time.zone.now)
     package.update_details
-    package.update_dependent_package_ids
+    # package.update_dependent_package_ids
     package.update_dependent_packages_count
     package.sync_maintainers_async
 
