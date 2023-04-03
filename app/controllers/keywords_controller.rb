e077894e069d84024482bebc20b43da2b8bcb53b
Andrew Nesbitt <andrewnez@gmail.com>
2023-04-03 17:46:42 +0100

    Sort keyword packages by avg rank

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -7,14 +7,11 @@ class KeywordsController < ApplicationController
     @keyword = params[:id]
 
     scope = Package.active.includes(:registry).where('keywords @> ARRAY[?]::varchar[]', @keyword)
-    sort = params[:sort].presence || 'packages.updated_at'
-    if params[:order] == 'asc'
-      scope = scope.order(Arel.sql(sort).asc.nulls_last)
-    else
-      scope = scope.order(Arel.sql(sort).desc.nulls_last)
-    end
+
+    @related_keywords = (scope.pluck(:keywords).flatten - [@keyword]).inject(Hash.new(0)) { |h, e| h[e] += 1; h }.sort_by { |_, v| -v }.first(100)
+
+    scope = scope.order(Arel.sql("(rankings->>'average')::text::float").asc)
     
     @pagy, @packages = pagy_countless(scope)
-    @related_keywords = (scope.pluck(:keywords).flatten - [@keyword]).inject(Hash.new(0)) { |h, e| h[e] += 1; h }.sort_by { |_, v| -v }.first(100)
   end
 end
