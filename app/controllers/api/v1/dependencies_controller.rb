dd956427addf99c5912c7b810260cc9c92efaf63
Andrew Nesbitt <andrewnez@gmail.com>
2023-04-12 16:30:38 +0100

    Allow filtering dependencies by ecosystem

--- app/controllers/api/v1/dependencies_controller.rb
+++ app/controllers/api/v1/dependencies_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::DependenciesController < Api::V1::ApplicationController
   def index
     scope = Dependency.all.includes(version: :package)
 
+    scope = scope.where(ecosystem: params[:ecosystem]) if params[:ecosystem].present?
     scope = scope.where(package_id: params[:package_id]) if params[:package_id].present?
     scope = scope.where(package_name: params[:package_name]) if params[:package_name].present?
     scope = scope.where(requirement: params[:requirement]) if params[:requirement].present?
