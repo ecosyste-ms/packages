64dc84e3f0abc8c24a6ca6f754eb6b2e63548e79
Andrew Nesbitt <andrewnez@gmail.com>
2023-05-18 17:16:31 +0100

    render readme and file list of latest version on packages#show page #353

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -1,4 +1,12 @@
 class VersionsController < ApplicationController
+
+  def index
+    @registry = Registry.find_by_name!(params[:registry_id])
+    @package = @registry.packages.find_by_name(params[:package_id])
+    @package = @registry.packages.find_by_name!(params[:package_id].downcase) if @package.nil?
+    @pagy, @versions = pagy_countless(@package.versions.order('published_at DESC, created_at DESC'))
+  end
+
   def show
     @registry = Registry.find_by_name!(params[:registry_id])
     @package = @registry.packages.find_by_name(params[:package_id])
