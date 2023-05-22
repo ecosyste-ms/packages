1c9fa93948743de66534bdbb935184a878223016
Andrew Nesbitt <andrewnez@gmail.com>
2023-05-22 11:37:42 +0100

    alpine v3.18

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,7 +42,7 @@ end
 
 alpine_registries = []
 # TODO automate version list from https://dl-cdn.alpinelinux.org/alpine/
-alpine_versions = ['edge', 'v3.17','v3.16','v3.15','v3.14','v3.13','v3.12', 'v3.11', 'v3.10', 'v3.9', 'v3.8', 'v3.7', 'v3.6', 'v3.5', 'v3.4', 'v3.3']
+alpine_versions = ['edge', 'v3.18', 'v3.17','v3.16','v3.15','v3.14','v3.13','v3.12', 'v3.11', 'v3.10', 'v3.9', 'v3.8', 'v3.7', 'v3.6', 'v3.5', 'v3.4', 'v3.3']
 
 alpine_versions.each do |version|
   repos = ['main', 'community']
