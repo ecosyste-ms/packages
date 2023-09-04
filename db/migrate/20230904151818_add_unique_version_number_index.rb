216fb407e2d041991604a55751bb04d58e7537e6
Andrew Nesbitt <andrewnez@gmail.com>
2023-09-04 22:26:23 +0100

    Tweak migration

--- db/migrate/20230904151818_add_unique_version_number_index.rb
+++ db/migrate/20230904151818_add_unique_version_number_index.rb
@@ -1,5 +1,8 @@
 class AddUniqueVersionNumberIndex < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction! 
+
   def change
-    add_index :versions, [:package_id, :number], unique: true
+    ActiveRecord::Base.connection.execute('SET statement_timeout TO 0')
+    add_index :versions, [:package_id, :number], unique: true, algorithm: :concurrently
   end
 end
