afaa5031b4cd4ba3a1be4ceb78f0fb8e9238b5a9
Andrew Nesbitt <andrewnez@gmail.com>
2023-09-07 17:26:30 +0100

    Skip syncing outdated docker packages in cron

--- app/models/package.rb
+++ app/models/package.rb
@@ -38,6 +38,8 @@ class Package < ApplicationRecord
   scope :with_issue_metadata, -> { where('length(issue_metadata::text) > 2') }
   scope :without_issue_metadata, -> { where(issue_metadata: nil) }
 
+  scope :not_docker, -> { where.not(ecosystem: 'docker') }
+
   after_create :update_rankings_async
 
   def self.keywords
@@ -45,11 +47,11 @@ class Package < ApplicationRecord
   end
 
   def self.sync_least_recent_async
-    Package.active.outdated.order('RANDOM()').limit(3000).select('id, last_synced_at').each(&:sync_async)
+    Package.active.outdated.not_docker.order('RANDOM()').limit(3000).select('id, last_synced_at').each(&:sync_async)
   end
 
   def self.sync_least_recent_top_async
-    Package.active.order('last_synced_at asc nulls first').top(2).where('last_synced_at < ?', 12.hours.ago).select('id, last_synced_at').limit(3_000).each(&:sync_async)
+    Package.active.not_docker.order('last_synced_at asc nulls first').top(2).where('last_synced_at < ?', 12.hours.ago).select('id, last_synced_at').limit(3_000).each(&:sync_async)
   end
 
   def self.check_statuses_async
