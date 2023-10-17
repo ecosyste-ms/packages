813c2cbecf81b43b009c21798b94d075d94c8879
Andrew Nesbitt <andrewnez@gmail.com>
2023-10-17 11:43:47 +0100

    deploy config for rails 7.1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.secret_key_base = '<%= ENV["SECRET_KEY_BASE"] %>'
 end
