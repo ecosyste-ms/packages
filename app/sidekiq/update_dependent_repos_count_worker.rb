cf8509d8d784b4ddea603295db6cf4247bf44bd3
Andrew Nesbitt <andrewnez@gmail.com>
2023-12-13 11:32:26 +0000

    disable dependent repos count job

--- app/sidekiq/update_dependent_repos_count_worker.rb
+++ app/sidekiq/update_dependent_repos_count_worker.rb
@@ -3,6 +3,7 @@ class UpdateDependentReposCountWorker
   sidekiq_options lock: :until_executed, queue: :low
 
   def perform(package_id)
-    Package.find_by_id(package_id).try(:update_dependent_repos_count)
+    # TODO noop empty whilst emptying the queue
+    # Package.find_by_id(package_id).try(:update_dependent_repos_count)
   end
 end
