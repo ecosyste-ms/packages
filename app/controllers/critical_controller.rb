cc3f8644148727e36f123832890337b36bb074a4
Andrew Nesbitt <andrewnez@gmail.com>
2024-02-25 16:56:36 +0000

    Refactor sorting logic in CriticalController and add sort options in index view

--- app/controllers/critical_controller.rb
+++ app/controllers/critical_controller.rb
@@ -4,7 +4,18 @@ class CriticalController < ApplicationController
 
     scope = scope.where(registry_id: Registry.find_by(name: params[:registry]).id) if params[:registry]
 
-    @pagy, @packages = pagy(scope.order('downloads DESC'))
-    @registries = Package.critical.group(:registry).count.sort_by{|r, c| -c}
+    if params[:sort].present? || params[:order].present?
+      sort = params[:sort].presence || 'updated_at'
+      if params[:order] == 'asc'
+        scope = scope.order(Arel.sql(sort).asc.nulls_last)
+      else
+        scope = scope.order(Arel.sql(sort).desc.nulls_last)
+      end
+    else
+      scope = scope.order('downloads DESC')
+    end
+
+    @pagy, @packages = pagy(scope)
+    @registries = Package.critical.group(:registry).count.sort_by{|r, c| r.name}
   end
 end
