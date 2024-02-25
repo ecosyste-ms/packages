c359cfe381ba05001d7f6bdc2bc15f27196507ec
Andrew Nesbitt <andrewnez@gmail.com>
2024-02-25 16:44:47 +0000

    Refactor critical_controller.rb to filter packages by registry if registry parameter is present

--- app/controllers/critical_controller.rb
+++ app/controllers/critical_controller.rb
@@ -1,7 +1,10 @@
 class CriticalController < ApplicationController
   def index
     scope = Package.critical.includes(:registry)
+
+    scope = scope.where(registry_id: Registry.find_by(name: params[:registry]).id) if params[:registry]
+
     @pagy, @packages = pagy(scope.order('downloads DESC'))
-    @registries = scope.group(:registry).count.sort_by{|r, c| -c}
+    @registries = Package.critical.group(:registry).count.sort_by{|r, c| -c}
   end
 end
