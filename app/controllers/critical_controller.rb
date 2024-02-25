d77b58e8969c6eff1a7ac8b425f8131da489eba8
Andrew Nesbitt <andrewnez@gmail.com>
2024-02-25 17:33:46 +0000

    Add maintainer filtering in critical controller and view

--- app/controllers/critical_controller.rb
+++ app/controllers/critical_controller.rb
@@ -4,6 +4,12 @@ class CriticalController < ApplicationController
 
     scope = scope.where(registry_id: Registry.find_by(name: params[:registry]).id) if params[:registry]
 
+    @maintainers = scope.map(&:maintainers).flatten.group_by{|m| m }.map{|m, packages| [m, packages.length]}.to_h.sort_by{|m, c| -c}.first(100)
+
+    if params[:maintainer].present?
+      scope = scope.joins(:maintainers).where(maintainers: {uuid: params[:maintainer]})
+    end
+
     if params[:sort].present? || params[:order].present?
       sort = params[:sort].presence || 'downloads'
       if params[:order] == 'asc'
@@ -16,6 +22,7 @@ class CriticalController < ApplicationController
     end
 
     @pagy, @packages = pagy(scope)
+
     @registries = Package.critical.group(:registry).count.sort_by{|r, c| r.name}
   end
 end
