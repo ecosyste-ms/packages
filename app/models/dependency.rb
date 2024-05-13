35311caea13cb575796a3a8b69a4942c91ad52e9
Andrew Nesbitt <andrewnez@gmail.com>
2024-05-13 14:36:03 +0100

    methods for finding and updating missing package_id field on Dependency

--- app/models/dependency.rb
+++ app/models/dependency.rb
@@ -8,4 +8,14 @@ class Dependency < ApplicationRecord
 
   scope :with_package, -> { where.not(package_id: nil) }
   scope :without_package, -> { where(package_id: nil) }
+
+  def find_package_id
+    Package.find_by(name: package_name, ecosystem: ecosystem).try(:id)
+  end
+
+  def update_package_id
+    return if package_id.present?
+    p_id = find_package_id
+    update(package_id: p_id) if p_id.present?
+  end
 end
