e25b070583ef51866eafd2bd7f68f8ba45237ace
Andrew Nesbitt <andrewnez@gmail.com>
2024-05-13 10:29:10 +0100

    Fix requirements filter in dependency api

--- app/controllers/api/v1/dependencies_controller.rb
+++ app/controllers/api/v1/dependencies_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::DependenciesController < Api::V1::ApplicationController
     scope = scope.where(ecosystem: params[:ecosystem]) if params[:ecosystem].present?
     scope = scope.where(package_id: params[:package_id]) if params[:package_id].present?
     scope = scope.where(package_name: params[:package_name]) if params[:package_name].present?
-    scope = scope.where(requirement: params[:requirement]) if params[:requirement].present?
+    scope = scope.where(requirements: params[:requirements]) if params[:requirements].present?
     scope = scope.where(kind: params[:kind]) if params[:kind].present?
     scope = scope.where(optional: params[:optional]) if params[:optional].present?
     scope = scope.where('dependencies.id > ?', params[:after]) if params[:after].present?
