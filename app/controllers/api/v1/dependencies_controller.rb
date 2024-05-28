b4a79cc23763522761574e8abe121885c65a49d4
Andrew Nesbitt <andrewnez@gmail.com>
2024-05-28 13:09:55 +0100

    Disable sorting in dependencies api controller

--- app/controllers/api/v1/dependencies_controller.rb
+++ app/controllers/api/v1/dependencies_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::DependenciesController < Api::V1::ApplicationController
   def index
-    scope = Dependency.all.includes(version: {package: :registry}).order('id asc')
+    scope = Dependency.all.includes(version: {package: :registry})
 
     scope = scope.where(ecosystem: params[:ecosystem]) if params[:ecosystem].present?
     scope = scope.where(package_id: params[:package_id]) if params[:package_id].present?
@@ -9,6 +9,15 @@ class Api::V1::DependenciesController < Api::V1::ApplicationController
     scope = scope.where(kind: params[:kind]) if params[:kind].present?
     scope = scope.where(optional: params[:optional]) if params[:optional].present?
     scope = scope.where('dependencies.id > ?', params[:after]) if params[:after].present?
+
+    if params[:sort].present?
+      sort = params[:sort].presence || 'id'
+      if params[:order] == 'asc'
+        scope = scope.order(Arel.sql(sort).asc.nulls_last)
+      else
+        scope = scope.order(Arel.sql(sort).desc.nulls_last)
+      end
+    end
 
     @pagy, @dependencies = pagy_countless(scope)
     fresh_when(etag: @dependencies.map(&:id), public: true)
