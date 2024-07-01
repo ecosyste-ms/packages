7452c7356ec0d12ab2fa8297dd97295b60ce4eb1
Andrew Nesbitt <andrewnez@gmail.com>
2024-07-01 11:25:03 +0100

    Method for quickly updating missing package_ids on mass

--- app/models/dependency.rb
+++ app/models/dependency.rb
@@ -18,6 +18,26 @@ class Dependency < ApplicationRecord
   def update_package_id
     return if package_id.present?
     p_id = find_package_id
-    update(package_id: p_id) if p_id.present?
+    update_column(:package_id, p_id) if p_id.present?
+  end
+
+  def self.update_missing_package_ids
+    registries = Registry.all.order('packages_count DESC').to_a
+    processed_packages = {}
+    without_package.find_each(order: :desc) do |dependency|
+      registry = registries.select { |r| r.ecosystem == dependency.ecosystem }.first
+      next unless registry
+
+      # Modify the cache key to include both the registry id and the package name
+      cache_key = "#{registry.id}_#{dependency.package_name}"
+
+      # Directly assign the result of the lookup to the cache, caching nil results as well
+      package_id = processed_packages[cache_key] = processed_packages.fetch(cache_key) do
+        registry.packages.find_by(name: dependency.package_name)&.id
+      end
+
+      next unless package_id
+      dependency.update_column(:package_id, package_id)
+    end
   end
 end
