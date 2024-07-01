29b1ec348d2c007db6cef5988bcf3cd4ab55903e
Andrew Nesbitt <andrewnez@gmail.com>
2024-07-01 10:59:36 +0100

    Speed up finding package ids for Dependency records

--- app/models/dependency.rb
+++ app/models/dependency.rb
@@ -10,7 +10,9 @@ class Dependency < ApplicationRecord
   scope :without_package, -> { where(package_id: nil) }
 
   def find_package_id
-    Package.find_by(name: package_name, ecosystem: ecosystem).try(:id)
+    registry = Registry.find_by_ecosystem(ecosystem)
+    return unless registry
+    registry.packages.find_by(name: package_name).try(:id)
   end
 
   def update_package_id
