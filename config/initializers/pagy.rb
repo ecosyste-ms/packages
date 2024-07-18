9d077e3627abf4683767c5c3e312ff12946b93bc
Andrew Nesbitt <andrewnez@gmail.com>
2024-07-18 14:56:54 +0100

    Disable size option in pagy

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -8,5 +8,4 @@ require 'pagy/extras/overflow'
 Pagy::DEFAULT[:limit] = 100
 Pagy::DEFAULT[:limit_param] = :per_page
 Pagy::DEFAULT[:limit_max] = 1000
-Pagy::DEFAULT[:size] = [1,2,2,1] 
 Pagy::DEFAULT[:overflow] = :empty_page
