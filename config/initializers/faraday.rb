b8661651955e7956f4104b8b05d068d55de87345
Andrew Nesbitt <andrewnez@gmail.com>
2025-07-07 19:11:05 +0100

    Set default User-Agent for Faraday connections

--- config/initializers/faraday.rb
+++ config/initializers/faraday.rb
@@ -1,2 +1,9 @@
 require 'faraday/typhoeus'
 Faraday.default_adapter = :typhoeus
+
+# Set default User-Agent for all Faraday connections
+Faraday.default_connection_options = {
+  headers: {
+    'User-Agent' => 'packages.ecosyste.ms'
+  }
+}
