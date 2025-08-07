1ebcec9207e34efc9aeab76f176d9f318e98352d
Andrew Nesbitt <andrewnez@gmail.com>
2025-08-07 12:14:01 +0100

    Update content security policy to allow 'unsafe-eval' in script sources

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,7 +1,7 @@
 SecureHeaders::Configuration.default do |config|
   config.csp = {
     default_src: %w('self'),
-    script_src: %w('self' 'unsafe-inline' https://cdnjs.cloudflare.com https://unpkg.com https://static.cloudflareinsights.com),
+    script_src: %w('self' 'unsafe-inline' 'unsafe-eval' https://cdnjs.cloudflare.com https://unpkg.com https://static.cloudflareinsights.com),
     style_src: %w('self' 'unsafe-inline' https://fonts.googleapis.com https://unpkg.com),
     font_src: %w('self' https://fonts.gstatic.com),
     img_src: %w('self' data: https:),
