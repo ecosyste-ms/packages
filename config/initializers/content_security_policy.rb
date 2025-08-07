d6a6dc9fabc4fadaa00700f2a148d30dd3bb4219
Andrew Nesbitt <andrewnez@gmail.com>
2025-08-07 11:49:12 +0100

    Update content security policy to include additional script and style sources

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,8 +1,8 @@
 SecureHeaders::Configuration.default do |config|
   config.csp = {
     default_src: %w('self'),
-    script_src: %w('self' 'unsafe-inline' https://cdnjs.cloudflare.com),
-    style_src: %w('self' 'unsafe-inline' https://fonts.googleapis.com),
+    script_src: %w('self' 'unsafe-inline' https://cdnjs.cloudflare.com https://unpkg.com https://static.cloudflareinsights.com),
+    style_src: %w('self' 'unsafe-inline' https://fonts.googleapis.com https://unpkg.com),
     font_src: %w('self' https://fonts.gstatic.com),
     img_src: %w('self' data: https:),
     connect_src: %w('self' *.ecosyste.ms)
