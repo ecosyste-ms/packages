3877e14100816461cbdb3d3ec2d39535fbc8a846
Andrew Nesbitt <andrewnez@gmail.com>
2025-10-02 11:39:57 +0100

    Remove unused Rails component configs from environment files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,14 +33,6 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  # config.active_storage.service = :local
-
-  # Don't care if the mailer can't send.
-  # config.action_mailer.raise_delivery_errors = false
-
-  # config.action_mailer.perform_caching = false
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -64,7 +56,4 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
-
-  # Uncomment if you wish to allow Action Cable access from any origin.
-  # config.action_cable.disable_request_forgery_protection = true
 end
