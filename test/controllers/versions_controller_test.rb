834e086619ed82c58b5b4a82e64a363fa45dfb18
Andrew Nesbitt <andrewnez@gmail.com>
2025-10-24 10:00:30 +0100

    Fix dependency sorting to handle nil kinds and add test for version dependencies

--- test/controllers/versions_controller_test.rb
+++ test/controllers/versions_controller_test.rb
@@ -21,4 +21,17 @@ class VersionsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_template 'versions/recent', file: 'versions/recent.html.erb'
   end
+
+  test 'get version with dependencies containing nil kind' do
+    @version.dependencies.create(package_name: 'dep1', ecosystem: 'cargo', requirements: '>= 1.0', kind: 'runtime')
+    @version.dependencies.create(package_name: 'dep2', ecosystem: 'cargo', requirements: '>= 2.0', kind: nil)
+    @version.dependencies.create(package_name: 'dep3', ecosystem: 'cargo', requirements: '>= 3.0', kind: 'development')
+
+    stub_request(:get, "https://archives.ecosyste.ms/api/v1/archives/list?url=https://crates.io/api/v1/crates/rand/1.0.0/download").
+      to_return(status: 200, body: file_fixture('list?url=https:%2F%2Fcrates.io%2Fapi%2Fv1%2Fcrates%2Frand%2F1.0.0%2Fdownload'))
+
+    get registry_package_version_path(registry_id: @registry.name, package_id: @package.name, id: '1.0.0')
+    assert_response :success
+    assert_template 'versions/show', file: 'versions/show.html.erb'
+  end
 end
