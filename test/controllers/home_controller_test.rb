037266d9738289e4bd4fe4f9ba05d44c15250998
Andrew Nesbitt <andrewnez@gmail.com>
2025-10-28 17:12:03 +0000

    Fix ecosystem link in index view to use correct registry association and add corresponding test

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -10,4 +10,36 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_template 'home/index'
   end
+
+  test 'links to ecosystem using ecosystem field not github field' do
+    # Clear existing registries to avoid interference
+    Registry.delete_all
+
+    # Create two alpine registries with different github and ecosystem values
+    r1 = Registry.create!(
+      name: 'alpine-v3.21',
+      url: 'https://pkgs.alpinelinux.org/packages?branch=v3.21',
+      ecosystem: 'alpine',
+      github: 'alpinelinux',
+      version: 'v3.21',
+      packages_count: 100,
+      default: false
+    )
+    r2 = Registry.create!(
+      name: 'alpine-v3.20',
+      url: 'https://pkgs.alpinelinux.org/packages?branch=v3.20',
+      ecosystem: 'alpine',
+      github: 'alpinelinux',
+      version: 'v3.20',
+      packages_count: 90,
+      default: false
+    )
+
+    get root_path
+    assert_response :success
+
+    # Verify that the link uses the ecosystem field (alpine) not the github field (alpinelinux)
+    assert_match /href="\/ecosystems\/alpine"/, response.body, "Should link to /ecosystems/alpine"
+    refute_match /href="\/ecosystems\/alpinelinux"/, response.body, "Should not link to /ecosystems/alpinelinux"
+  end
 end
