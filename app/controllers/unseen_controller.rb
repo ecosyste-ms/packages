57d2ebb02c51f9ce4b5d3984e7ec4f8136f93552
Andrew Nesbitt <andrewnez@gmail.com>
2025-11-13 11:59:46 +0000

    Fix unseen controller

--- app/controllers/unseen_controller.rb
+++ app/controllers/unseen_controller.rb
@@ -1,10 +1,10 @@
 class UnseenController < ApplicationController
   def index
-    @scope = Package.includes(:registry).with_repo_metadata.order('downloads DESC').where('downloads > ?', 100_000).where("(repo_metadata ->> 'stargazers_count')::text::integer < 100")
+    @scope = Package.includes(:registry).with_repo_metadata.order('packages.downloads DESC').where('packages.downloads > ?', 100_000).where("(repo_metadata ->> 'stargazers_count')::text::integer < 100")
 
     @all_registries = Registry.all
     @registry_ids = @scope.pluck(:registry_id).tally
-    @registries = @registry_ids.map{|id, count| [@all_registries.find{|r| r.id == id}, count]}.sort_by{|r, count| -count}
+    @registries = @registry_ids.map{|id, count| [@all_registries.find{|r| r.id == id}, count]}.compact.reject{|r, count| r.nil?}.sort_by{|r, count| -count}
 
     if params[:registry]
       @registry = Registry.find_by_name!(params[:registry])
@@ -16,7 +16,8 @@ class UnseenController < ApplicationController
 
   def ecosystem
     @ecosystem = params[:ecosystem]
-    @registry = Registry.where(ecosystem: @ecosystem).first
+    @registry = Registry.find_by(ecosystem: @ecosystem) || Registry.find_by(name: @ecosystem)
+    raise ActiveRecord::RecordNotFound, "Registry with ecosystem or name '#{@ecosystem}' not found" if @registry.nil?
     redirect_to unseen_path(registry: @registry.name)
   end
 end
---
+++ test/controllers/unseen_controller_test.rb
@@ -0,0 +1,84 @@
+require 'test_helper'
+
+class UnseenControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @registry = Registry.create(name: 'npmjs.org', url: 'https://npmjs.org', ecosystem: 'npm')
+
+    @unseen_package = @registry.packages.create(
+      ecosystem: 'npm',
+      name: 'unseen-package',
+      downloads: 150_000,
+      repo_metadata: {
+        'stargazers_count' => 50
+      }
+    )
+
+    @popular_package = @registry.packages.create(
+      ecosystem: 'npm',
+      name: 'popular-package',
+      downloads: 200_000,
+      repo_metadata: {
+        'stargazers_count' => 500
+      }
+    )
+
+    @low_download_package = @registry.packages.create(
+      ecosystem: 'npm',
+      name: 'low-download-package',
+      downloads: 50_000,
+      repo_metadata: {
+        'stargazers_count' => 10
+      }
+    )
+  end
+
+  test 'should get index' do
+    get unseen_path
+    assert_response :success
+  end
+
+  test 'should show packages with high downloads and low stars' do
+    get unseen_path
+    assert_response :success
+    assert_includes response.body, @unseen_package.name
+  end
+
+  test 'should not show packages with high stars' do
+    get unseen_path
+    assert_response :success
+    assert_not_includes response.body, @popular_package.name
+  end
+
+  test 'should not show packages with low downloads' do
+    get unseen_path
+    assert_response :success
+    assert_not_includes response.body, @low_download_package.name
+  end
+
+  test 'should filter by registry' do
+    other_registry = Registry.create(name: 'pypi', url: 'https://pypi.org', ecosystem: 'pypi')
+    other_package = other_registry.packages.create(
+      ecosystem: 'pypi',
+      name: 'other-package',
+      downloads: 200_000,
+      repo_metadata: {
+        'stargazers_count' => 30
+      }
+    )
+
+    get unseen_path(registry: @registry.name)
+    assert_response :success
+    assert_includes response.body, @unseen_package.name
+    assert_not_includes response.body, other_package.name
+  end
+
+  test 'should redirect from ecosystem to registry name' do
+    get unseen_ecosystem_path(ecosystem: 'npm')
+    assert_redirected_to unseen_path(registry: @registry.name)
+  end
+
+  test 'should return 404 when ecosystem not found' do
+    get unseen_ecosystem_path(ecosystem: 'invalid-ecosystem')
+    assert_response :not_found
+  end
+end
