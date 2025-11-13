5e0677b0ac4d591144f931c1dcfbd64da8e0b8aa
Andrew Nesbitt <andrewnez@gmail.com>
2025-11-13 13:00:58 +0000

    Set statement timeout to 0 for stargazers index migration

--- db/migrate/20251113124934_add_stargazers_index.rb
+++ db/migrate/20251113124934_add_stargazers_index.rb
@@ -1,5 +1,6 @@
 class AddStargazersIndex < ActiveRecord::Migration[8.1]
   def change
+    ActiveRecord::Base.connection.execute('SET statement_timeout TO 0')
     add_index :packages,
               "(((repo_metadata ->> 'stargazers_count')::text::integer)) DESC NULLS LAST",
               name: "index_packages_on_stargazers_desc",
