5d7041a9f2db23748aedb28bf733ca0d4136a9d2
Andrew Nesbitt <andrewnez@gmail.com>
2026-01-27 13:16:32 +0000

    Remove slow unique packages query from ecosystem show page
    The distinct count query on packages was too slow for large ecosystems
    like nixpkgs. The total packages count from cached registry counters
    is still displayed.

--- app/controllers/ecosystems_controller.rb
+++ app/controllers/ecosystems_controller.rb
@@ -5,7 +5,6 @@ class EcosystemsController < ApplicationController
 
   def show
     @registries = Registry.where(ecosystem: params[:id]).order('packages_count desc, name desc').all
-    @unique_packages_count = Package.where(registry_id: @registries.map(&:id)).distinct.count(:name)
     raise ActiveRecord::RecordNotFound if @registries.empty?
   end
 end
