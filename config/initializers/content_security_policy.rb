2b89a4bce356a74d478693f71a76140d2c2bbdfd
Andrew Nesbitt <andrewnez@gmail.com>
2026-03-05 09:55:57 +0000

    Update content security policy to include media.ethicalads.io and server.ethicalads.io

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,10 +1,10 @@
 SecureHeaders::Configuration.default do |config|
   config.csp = {
     default_src: %w('self'),
-    script_src: %w('self' 'unsafe-inline' 'unsafe-eval' https://cdnjs.cloudflare.com https://unpkg.com https://static.cloudflareinsights.com),
+    script_src: %w('self' 'unsafe-inline' 'unsafe-eval' https://cdnjs.cloudflare.com https://unpkg.com https://static.cloudflareinsights.com https://media.ethicalads.io),
     style_src: %w('self' 'unsafe-inline' https://fonts.googleapis.com https://unpkg.com),
     font_src: %w('self' https://fonts.gstatic.com),
     img_src: %w('self' data: https:),
-    connect_src: %w('self' *.ecosyste.ms)
+    connect_src: %w('self' *.ecosyste.ms https://server.ethicalads.io)
   }
 end
