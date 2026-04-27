a13ba13bfdcc0abb353ef670a83101a312cc82eb
Andrew Nesbitt <andrewnez@gmail.com>
2026-04-27 12:26:39 +0100

    Remove ethical ads script from layout and update content security policy to exclude ethical ads sources

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,10 +1,9 @@
 SecureHeaders::Configuration.default do |config|
   config.csp = {
     default_src: %w('self'),
-    script_src: %w('self' 'unsafe-inline' 'unsafe-eval' https://cdnjs.cloudflare.com https://unpkg.com https://static.cloudflareinsights.com https://media.ethicalads.io https://server.ethicalads.io),
+    script_src: %w('self' 'unsafe-inline' 'unsafe-eval' https://cdnjs.cloudflare.com https://unpkg.com https://static.cloudflareinsights.com),
     style_src: %w('self' 'unsafe-inline' https://fonts.googleapis.com https://unpkg.com),
     font_src: %w('self' https://fonts.gstatic.com),
     img_src: %w('self' data: https:),
-    connect_src: %w('self' *.ecosyste.ms https://server.ethicalads.io)
   }
 end
