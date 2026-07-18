47934c4d16ced2152df214218aee0fd2b8963f80
abhinavgautam01 <abgautam1017@gmail.com>
2026-07-18 12:36:10 +0530

    Support configured self-hosted forge URLs

--- app/services/forge_url_parser.rb
+++ app/services/forge_url_parser.rb
@@ -1,19 +1,50 @@
 class ForgeUrlParser < UrlParser
-  HOSTS = %w[codeberg.org gitea.com].freeze
-  HOST_PATTERN = HOSTS.map { |host| Regexp.escape(host) }.join('|').freeze
+  Host = Data.define(:hostname, :port, :path_prefix) do
+    def authority
+      port ? "#{hostname}:#{port}" : hostname
+    end
+
+    def base_url
+      path = path_prefix.any? ? "/#{path_prefix.join('/')}" : ''
+      "https://#{authority}#{path}"
+    end
+  end
+
+  DEFAULT_HOSTS = %w[https://codeberg.org https://gitea.com].freeze
+
+  def self.hosts
+    (DEFAULT_HOSTS + configured_hosts).filter_map { |url| build_host(url) }.uniq(&:base_url)
+  end
+
+  def self.configured_hosts
+    ENV.fetch('FORGE_HOSTS', '').split(',').map(&:strip)
+  end
+
+  def self.build_host(url)
+    uri = URI.parse(url)
+    return unless uri.is_a?(URI::HTTPS) && uri.host.present? && uri.userinfo.nil? && uri.query.nil? && uri.fragment.nil?
+
+    Host.new(
+      hostname: uri.host.downcase,
+      port: uri.port == uri.default_port ? nil : uri.port,
+      path_prefix: uri.path.split('/').reject(&:blank?)
+    )
+  rescue URI::InvalidURIError
+    nil
+  end
 
   private
 
   def full_domain
-    "https://#{matched_host}"
+    host_config.base_url
   end
 
   def parseable?
-    matched_host.present?
+    host_config.present?
   end
 
   def includes_domain?
-    matched_host.present?
+    host_config.present?
   end
 
   def extractable_early?
@@ -21,12 +52,24 @@ class ForgeUrlParser < UrlParser
   end
 
   def remove_domain
-    url.gsub!(/(?:#{HOST_PATTERN})(?::|\/)?/i, '')
+    url.gsub!(/#{Regexp.escape(host_config.authority)}(?::|\/)?/i, '')
   end
 
-  def matched_host
-    @matched_host ||= url.gsub(/\s/, '').match(
-      /(?:\A|\/\/|@|(?:git|ssh|https?|hg|svn|scm):)(?:(?:www|ssh|raw|git|wiki)\.)?(#{HOST_PATTERN})(?=[:\/]|\z)/i
-    )&.[](1)&.downcase
+  def remove_extra_segments
+    segments = url.dup.split('/').reject(&:blank?)
+    path_prefix = host_config.path_prefix
+    return self.url = [] unless segments.first(path_prefix.length) == path_prefix
+
+    self.url = segments.drop(path_prefix.length).first(2)
+  end
+
+  def host_config
+    @host_config ||= self.class.hosts.find do |host|
+      url.gsub(/\s/, '').match?(authority_pattern(host))
+    end
+  end
+
+  def authority_pattern(host)
+    /(?:\A|\/\/|@|(?:git|ssh|https?|hg|svn|scm):)(?:(?:www|ssh|raw|git|wiki)\.)?#{Regexp.escape(host.authority)}(?=[:\/]|\z)/i
   end
 end
--- test/services/forge_url_parser_test.rb
+++ test/services/forge_url_parser_test.rb
@@ -25,4 +25,30 @@ class ForgeUrlParserTest < ActiveSupport::TestCase
       assert_nil ForgeUrlParser.parse(url)
     end
   end
+
+  test 'parses configured self-hosted forge urls' do
+    with_forge_hosts('https://gitea.example.com,https://forgejo.example.com/forgejo') do
+      assert_equal 'org/repo', ForgeUrlParser.parse('https://gitea.example.com/org/repo')
+      assert_equal 'group/project', ForgeUrlParser.parse('https://forgejo.example.com/forgejo/group/project')
+      assert_equal 'https://forgejo.example.com/forgejo/group/project', ForgeUrlParser.parse_to_full_url('https://forgejo.example.com/forgejo/group/project')
+      assert_nil ForgeUrlParser.parse('https://forgejo.example.com/group/project')
+    end
+  end
+
+  test 'ignores invalid configured forge hosts' do
+    with_forge_hosts('gitea.example.com,https://forgejo.example.com?source=configuration') do
+      assert_nil ForgeUrlParser.parse('https://gitea.example.com/org/repo')
+      assert_nil ForgeUrlParser.parse('https://forgejo.example.com/group/project')
+    end
+  end
+
+  private
+
+  def with_forge_hosts(hosts)
+    original_hosts = ENV['FORGE_HOSTS']
+    ENV['FORGE_HOSTS'] = hosts
+    yield
+  ensure
+    original_hosts.nil? ? ENV.delete('FORGE_HOSTS') : ENV['FORGE_HOSTS'] = original_hosts
+  end
 end
