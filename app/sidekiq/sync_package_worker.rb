89703a01816536898043e685670f90078762b277
abhinavgautam01 <abgautam1017@gmail.com>
2026-07-27 00:33:27 +0530

    Simplify forced package sync dispatch

--- app/sidekiq/sync_package_worker.rb
+++ app/sidekiq/sync_package_worker.rb
@@ -6,6 +6,6 @@ class SyncPackageWorker
     registry = Registry.find_by_id(registry_id)
     return if registry.nil? || registry.sync_in_batches?
 
-    force ? registry.sync_package(name, force: true) : registry.sync_package(name)
+    registry.sync_package(name, force: force)
   end
 end
--- test/sidekiq/sync_package_worker_test.rb
+++ test/sidekiq/sync_package_worker_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class SyncPackageWorkerTest < ActiveSupport::TestCase
   test 'perform' do
     @registry = Registry.create(name: 'Rubygems.org', url: 'https://rubygems.org', ecosystem: 'rubygems')
-    @registry.expects(:sync_package).with('foo')
+    @registry.expects(:sync_package).with('foo', force: false)
     Registry.expects(:find_by_id).with(@registry.id).returns(@registry)
     job = SyncPackageWorker.new
     job.perform(@registry.id, 'foo')
