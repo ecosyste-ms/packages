b37e5e9b42b670ffa7b682a18a91476b36e4af34
Andrew Nesbitt <andrewnez@gmail.com>
2026-08-12 15:46:24 +0100

    Only walk tags for action.yml when default branch has none
    fetch_package_metadata_uncached fetched action.yml from the default
    branch, then unconditionally set yaml = nil and walked up to 1000
    tags probing .yml then .yaml on each until one was found. The TODO
    comment says to search tags only when the default branch does not
    have the file, but the code discarded the default-branch result.
    
    Now the tags fetch and walk only happen when the default-branch
    probe came back blank, saving one repos.ecosyste.ms tags call and
    at least one raw.githubusercontent.com fetch per successful actions
    sync, and eliminating the tag walk entirely for the common case.

--- test/models/ecosystem/actions_test.rb
+++ test/models/ecosystem/actions_test.rb
@@ -174,7 +174,26 @@ class ActionsTest < ActiveSupport::TestCase
       :homepage=>nil, 
       :tags_url=>"http://repos.ecosyste.ms/api/v1/hosts/GitHub/repositories/getsentry%2Faction-git-diff-suggestions/tags", 
       :namespace=>"getsentry", 
-      :metadata=>{"name"=>"action-git-diff-suggestions", "description"=>"This GitHub Action will take the current git changes and apply them as GitHub code review suggestions", "author"=>"Sentry", "branding"=>{"icon"=>"book-open", "color"=>"purple"}, "inputs"=>{"github-token"=>{"description"=>"github token"}, "message"=>{"description"=>"The message to prepend the review suggestion"}}, "runs"=>{"using"=>"node12", "main"=>"dist/index.js"}, "default_branch"=>"main", "path"=>nil}}
+      :metadata=>{"name"=>"action-git-diff-suggestions", "description"=>"This GitHub Action will take the current git changes and apply them as GitHub code review suggestions", "author"=>"Sentry", "branding"=>{"icon"=>"book-open", "color"=>"purple"}, "inputs"=>{"github-token"=>{"description"=>"github token", "default"=>"${{ github.token }}"}, "message"=>{"description"=>"The message to prepend the review suggestion"}}, "runs"=>{"using"=>"node12", "main"=>"dist/index.js"}, "default_branch"=>"main", "path"=>nil}}
+    assert_not_requested :get, "http://repos.ecosyste.ms/api/v1/hosts/GitHub/repositories/getsentry%2Faction-git-diff-suggestions/tags?per_page=1000"
+    assert_not_requested :get, "https://raw.githubusercontent.com/getsentry/action-git-diff-suggestions/v1/action.yml"
+  end
+
+  test 'package_metadata falls back to tags when default branch has no action file' do
+    stub_request(:get, "https://repos.ecosyste.ms/api/v1/repositories/lookup?url=https://github.com/getsentry/action-git-diff-suggestions")
+      .to_return({ status: 200, body: file_fixture('actions/lookup?url=https:%2F%2Fgithub.com%2Fgetsentry%2Faction-git-diff-suggestions') })
+    stub_request(:get, "https://raw.githubusercontent.com/getsentry/action-git-diff-suggestions/main/action.yml")
+      .to_return({ status: 404 })
+    stub_request(:get, "https://raw.githubusercontent.com/getsentry/action-git-diff-suggestions/main/action.yaml")
+      .to_return({ status: 404 })
+    stub_request(:get, "http://repos.ecosyste.ms/api/v1/hosts/GitHub/repositories/getsentry%2Faction-git-diff-suggestions/tags?per_page=1000")
+      .to_return({ status: 200, body: file_fixture('actions/tags') })
+    stub_request(:get, "https://raw.githubusercontent.com/getsentry/action-git-diff-suggestions/v1/action.yml")
+      .to_return({ status: 200, body: file_fixture('actions/action.yml.1') })
+
+    package_metadata = @ecosystem.package_metadata('getsentry/action-git-diff-suggestions')
+    assert_equal 'getsentry/action-git-diff-suggestions', package_metadata[:name]
+    assert_equal 'node12', package_metadata[:metadata]['runs']['using']
   end
 
   test 'versions_metadata' do
